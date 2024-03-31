f0de7e2e9edb2f37023c4d4446108dd038df50b1
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2024-03-31 10:26:11 +0200

    Delete workshop and corresponding feedbacks api

--- Back/Workshop/src/main/java/org/dolymy/workshop/services/WorkshopService.java
+++ Back/Workshop/src/main/java/org/dolymy/workshop/services/WorkshopService.java
@@ -2,6 +2,7 @@ package org.dolymy.workshop.services;
 
 import lombok.RequiredArgsConstructor;
 import org.dolymy.workshop.entities.Workshop;
+import org.dolymy.workshop.repositories.FeedbackRepository;
 import org.dolymy.workshop.repositories.WorkshopRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +22,7 @@ public class WorkshopService {
     private static final String ERROR_UPDATE= "Error occurred while updating";
 
     private final WorkshopRepository workshopRepository;
+    private final FeedbackRepository feedbackRepository;
 
 
     public Workshop saveWorkshop(Workshop workshop) {
@@ -30,6 +33,12 @@ public class WorkshopService {
         return this.workshopRepository.findAll();
     }
 
+    /**
+     * Retrieves a workshop by its ID.
+     *
+     * @param id The ID of the workshop to retrieve.
+     * @return The workshop with the specified ID if found, or null if not found.
+     */
     public Workshop findWorkshopById(String id) {
         Workshop workshop =null;
         if(id != null){
@@ -47,6 +56,14 @@ public class WorkshopService {
 
         return workshop;
     }
+
+    /**
+     * Updates an existing workshop with the provided ID.
+     *
+     * @param id             The ID of the workshop to update.
+     * @param updatedWorkshop The updated workshop object.
+     * @return The updated workshop if found, or a workshop object with title and description "NOT_FOUND" if the workshop with the specified ID is not found.
+     */
 
     public Workshop updateWorkshop(String id, Workshop updatedWorkshop) {
         Optional<Workshop> existingWorkshop = this.workshopRepository.findById(id);
@@ -68,10 +85,24 @@ public class WorkshopService {
                 .build();
     }
 
+    /**
+     * Deletes a workshop and its corresponding feedbacks.
+     *
+     * @param id The ID of the workshop to be deleted.
+     */
     public void deleteWorkshop(String id) {
         if (id!= null)
         {
-           if(this.workshopRepository.findById(id).isPresent()) this.workshopRepository.deleteById(id);
+            Optional<Workshop> workshop = this.workshopRepository.findById(id);
+           if(workshop.isPresent()){
+               //delete workshop
+               this.workshopRepository.deleteById(id);
+               //delete feedbacks corresponding to this workshop
+               workshop.get().getFeedbacks().stream()
+                        .forEach(feedbackRepository::delete);
+
+
+           }
            else LOG.error(ERROR_NON_PRESENT_ID, id);
         }
         else LOG.error(ERROR_NULL_ID);
