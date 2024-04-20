be32a4ca1ca16613f7773708cd9cc73ae944df57
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2024-04-20 23:55:01 +0200

    adding workshop and feedback classes

--- Front/SwifthubFront/src/app/components/workshop/show-workshop/show-workshop.component.ts
+++ Front/SwifthubFront/src/app/components/workshop/show-workshop/show-workshop.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-show-workshop',
   templateUrl: './show-workshop.component.html',
   styleUrls: ['./show-workshop.component.scss']
 })
-export class ShowWorkshopComponent {
+export class ShowWorkshopComponent implements OnInit {
+
+  constructor(){}
+
+  ngOnInit(): void {
+   
+    
+  }
 
 }
---
+++ Front/SwifthubFront/src/app/models/workshop/workshop.ts
@@ -0,0 +1,14 @@
+import { Feedback } from "../feedback/feedback";
+
+export class Workshop {
+    workshop_id!: string;
+    title!: string;
+    description!: string;
+    capacity!: number;
+    start_date!: Date;
+    end_date!: Date;
+    location!: string;
+    link!: string;
+    userId!:string;
+    feedbacks!: Feedback[];
+}
