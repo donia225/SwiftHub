20fecf69579e2a39d90cb13bdc57f579fce33b45
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2024-04-21 13:57:56 +0200

    add-workshop component

---
+++ Front/SwifthubFront/src/app/components/workshop/add-workshop/add-workshop.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-workshop',
+  templateUrl: './add-workshop.component.html',
+  styleUrls: ['./add-workshop.component.scss']
+})
+export class AddWorkshopComponent {
+
+}
