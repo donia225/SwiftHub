227b130261846e61cfe93ba4e4423445dd237579
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2024-05-04 00:21:49 +0200

    Revert "notif added to front"
    This reverts commit 6d7eeddf384a2eb223cd6885a77f0d7ccf47553a.

--- Front/SwifthubFront/src/app/components/feedback/admin-show-feedback/admin-show-feedback.component.ts
+++ Front/SwifthubFront/src/app/components/feedback/admin-show-feedback/admin-show-feedback.component.ts
@@ -1,34 +1,10 @@
-// import { Component, OnInit } from '@angular/core';
-// import { Notification } from 'src/app/models/feedbackNotification/notification';
-// import { NotificationFeedbackService } from 'src/app/services/notifications/notification-feedback.service';
+import { Component } from '@angular/core';
 
-// @Component({
-//   selector: 'app-admin-show-feedback',
-//   templateUrl: './admin-show-feedback.component.html',
-//   styleUrls: ['./admin-show-feedback.component.scss']
-// })
-// export class AdminShowFeedbackComponent implements OnInit {
+@Component({
+  selector: 'app-admin-show-feedback',
+  templateUrl: './admin-show-feedback.component.html',
+  styleUrls: ['./admin-show-feedback.component.scss']
+})
+export class AdminShowFeedbackComponent {
 
-//   notification!:Notification;
-//   constructor(private notifService:NotificationFeedbackService){}
-
-//   ngOnInit(): void {
-//     // opening cnx with server socket 
-//     let stompClient= this.notifService.connect();
-//     stompClient.connect({},()=>{
-//       //add notif to topic 
-//       stompClient.subscribe('/topic/notification', notifications=>{
-//         //get recent message 
-//         this.notification=JSON.parse(notifications.body).message;
-//       })
-//     });
-//   }
-
-  
- 
-
-
-
-
-
-// }
+}
