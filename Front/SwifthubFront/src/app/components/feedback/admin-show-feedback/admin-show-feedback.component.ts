b8647858e513d306594164555504cd9ed238b58e
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2024-05-04 22:48:45 +0200

    added notif

--- Front/SwifthubFront/src/app/components/feedback/admin-show-feedback/admin-show-feedback.component.ts
+++ Front/SwifthubFront/src/app/components/feedback/admin-show-feedback/admin-show-feedback.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NotificationFeedbackService } from 'src/app/services/notifications/notification-feedback.service';
 
 @Component({
   selector: 'app-admin-show-feedback',
   templateUrl: './admin-show-feedback.component.html',
   styleUrls: ['./admin-show-feedback.component.scss']
 })
-export class AdminShowFeedbackComponent {
+export class AdminShowFeedbackComponent implements OnInit {
+
+  notification!:Notification;
+
+  constructor(private notifService:NotificationFeedbackService){}
+
+  ngOnInit(): void {
+
+      // opening cnx with server socket 
+      let stompClient= this.notifService.connect();
+      stompClient.connect({}, (frame:any)=>{
+        //add notif to topic 
+        stompClient.subscribe('/topic/notification', (notification:any)=>{
+          //get recent message 
+          this.notification=JSON.parse(notification.body).message;
+        })
+      });
+
+
+  }
 
 }
--- Front/SwifthubFront/src/app/services/notifications/notification-feedback.service.ts
+++ Front/SwifthubFront/src/app/services/notifications/notification-feedback.service.ts
@@ -1,23 +1,28 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
 
-var sockJS= require("sockjs-client");
-var Stomp= require("stompjs");
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationFeedbackService {
 
-  protected wsUrl = `${environment.API_URL}/api/workshop/socket`;
-  constructor() { }
+  protected wsUrl = `${environment.API_URL}/api/workshop/notif`;
+
+  constructor() {
+
+   }
 
   // oppening cnx with backendSocket 
   public connect(){
-    let socket=new sockJS(this.wsUrl);
+    let socket=new SockJS(this.wsUrl);
     let stompClient=Stomp.over(socket)
     return stompClient;
   }
+
 
 
 
