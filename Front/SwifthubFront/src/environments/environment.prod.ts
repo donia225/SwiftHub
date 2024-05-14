b394e6ee600d10afc204a28533a47574ad70c6e5
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2024-05-14 19:33:09 +0200

    built the angular image

--- Front/SwifthubFront/src/environments/environment.prod.ts
+++ Front/SwifthubFront/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL1: 'http://user-service:8050',
+  API_URL: 'http://gateway-service:8222',
+  apiBaseUrl: 'http://gateway-service:8222/api/request'
 };
--- Front/SwifthubFront/src/environments/environment.ts
+++ Front/SwifthubFront/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   API_URL1: 'http://user-service:8050',
   API_URL: 'http://gateway-service:8222',
   apiBaseUrl: 'http://gateway-service:8222/api/request'
